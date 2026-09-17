323d7a4766d0a4f29ebf8bdd764e06995a02cec0
longhe Zhang <z11310082@gmail.com>
2026-09-17 10:50:20 +0800

    Redesign academic homepage (V3)

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const header = document.querySelector("[data-header]");
-
 function syncHeader() {
-  header.classList.toggle("is-scrolled", window.scrollY > 24);
+  header?.classList.toggle("is-scrolled", window.scrollY > 12);
 }
-
 syncHeader();
 window.addEventListener("scroll", syncHeader, { passive: true });
